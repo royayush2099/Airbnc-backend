3f699942bf1deab2c23e6a00cf553c7a800d776a
Ayushroy <royayush2099@gmail.com>
2024-10-02 13:08:38 +0530

    adding mongo

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express =require('express');
+const cors = require('cors')
+const mongoose= require('mongoose')
+require('dotenv').config()
 const app = express();
+app.use(express.json());// it is used to parse the json res object as we need to destrucuture the req body from frontend
+app.use(cors({
+    credentials:true,
+    origin: 'http://localhost:5173',
+}));
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
+
 const Port = 8000;
 app.get('/test',(req,res)=>{
 res.json('test ok')
 });
+
+app.post('/register',(req,res)=>{
+    const {name,email,password} = req.body;
+    res.json({name,email,password});
+})
 
 app.listen(Port)
